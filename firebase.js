8a58120e4c61a48c8b6b460b13a82fc4e636f5ab
gavishori <gavishori@gmail.com>
2025-10-13 10:22:24 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,13 +1,6 @@
 // Unified Firebase wrapper exposing the exact names script.js expects.
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import {
-  getAuth,
-  onAuthStateChanged,
-  signOut,
-  signInWithEmailAndPassword,
-  createUserWithEmailAndPassword,
-  sendPasswordResetEmail
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import {getAuth, onAuthStateChanged, signOut, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, setPersistence, browserLocalPersistence} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import {
   initializeFirestore,
   setLogLevel,
@@ -46,6 +39,8 @@ setLogLevel("error");
 
 // --- AUTH ---
 export const auth = getAuth(app);
+try { setPersistence(auth, browserLocalPersistence); } catch(e) {}
+
 // Convenience named exports (used in a few places)
 export const onAuth = onAuthStateChanged;
 export const signOutUser = () => signOut(auth);
