60c0744a0abc1821c0973116258381f54985a761
gavishori <gavishori@gmail.com>
2025-10-21 04:55:15 +0300

    Delete app directory

--- app/firebase.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { firebaseConfig } from "./firebase-config.js";
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
