897790f1c0fc28c1a231c5f933a817bf4b27ceaf
gavishori <gavishori@gmail.com>
2026-05-28 13:29:45 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3709,12 +3709,12 @@ function lockExpenseMetaRowInline(){
   if(!row || !dateCol || !timeCol || !locCol) return;
   const imp = (el, prop, value)=> el?.style?.setProperty(prop, value, 'important');
   [
-    ['display','grid'], ['grid-template-columns','minmax(0, 1fr) 75px 76px'], ['grid-template-rows','34px'],
+    ['display','grid'], ['grid-template-columns','128px 66px 60px'], ['grid-template-rows','34px'],
     ['grid-auto-rows','0'], ['grid-auto-flow','unset'], ['align-items','start'], ['justify-content','end'],
     ['column-gap','6px'], ['row-gap','0'], ['height','34px'], ['min-height','34px'], ['max-height','34px'],
     ['overflow','visible'], ['direction','rtl'], ['width','100%'], ['max-width','100%']
   ].forEach(([p,v])=> imp(row,p,v));
-  [[dateCol,'1','100%','0','100%','34px'], [timeCol,'2','75px','75px','75px','34px'], [locCol,'3','76px','76px','76px','34px']].forEach(([el,col,w,minW,maxW,h])=>{
+  [[dateCol,'1','128px','128px','128px','34px'], [timeCol,'2','66px','66px','66px','34px'], [locCol,'3','60px','60px','60px','34px']].forEach(([el,col,w,minW,maxW,h])=>{
     imp(el,'grid-column',col); imp(el,'grid-row','1'); imp(el,'width',w); imp(el,'min-width',minW); imp(el,'max-width',maxW);
     imp(el,'height',h); imp(el,'min-height',h); imp(el,'max-height',h); imp(el,'margin','0'); imp(el,'overflow','hidden');
     imp(el,'font-size','0'); imp(el,'line-height','0'); imp(el,'gap','0'); imp(el,'padding','0');
@@ -3731,7 +3731,7 @@ function lockExpenseMetaRowInline(){
     imp(el,'border-radius','9px'); imp(el,'background','#fff'); imp(el,'box-shadow','none');
     imp(el,'transform','none'); imp(el,'margin','0');
   });
-  imp(locBtn,'width','76px'); imp(locBtn,'min-width','76px'); imp(locBtn,'max-width','76px');
+  imp(locBtn,'width','60px'); imp(locBtn,'min-width','60px'); imp(locBtn,'max-width','60px');
   imp(locBtn,'height','32px'); imp(locBtn,'min-height','32px'); imp(locBtn,'max-height','32px');
   imp(locBtn,'padding','0'); imp(locBtn,'border','1px solid #d9e1ea'); imp(locBtn,'border-radius','9px');
   imp(locBtn,'background','#fff'); imp(locBtn,'box-shadow','none');
